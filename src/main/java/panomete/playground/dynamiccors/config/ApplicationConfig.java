3471c3791cc2fd89af871be0af28237e6db47933
oat431 <oat431@gmail.com>
2023-10-12 03:38:10 +0700

    🔨 add: allowed single cors

---
+++ src/main/java/panomete/playground/dynamiccors/config/ApplicationConfig.java
@@ -0,0 +1,47 @@
+package panomete.playground.dynamiccors.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class ApplicationConfig {
+
+    private final String[] ALLOWED_ORIGINS = {
+            "http://localhost:3000"
+    };
+
+    private final String[] ALLOWED_METHOD = {
+            "GET", "POST", "PUT", "DELETE", "OPTIONS"
+    };
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of(ALLOWED_ORIGINS));
+        configuration.setAllowedMethods(List.of(ALLOWED_METHOD));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setExposedHeaders(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins(ALLOWED_ORIGINS);
+            }
+        };
+    }
+}
